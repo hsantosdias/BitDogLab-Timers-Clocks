8320bd635214cdf86889d4a7d176656ed068459f
hsantosdias@gmail.com <hsantosdias@gmail.com>
2025-02-01 18:51:22 -0300

    Codigo Blink de teste

--- BitDogLab-Timers-Clocks.c
+++ BitDogLab-Timers-Clocks.c
@@ -2,16 +2,21 @@
 #include "pico/stdlib.h"
 
 
+int gpio_pin = 13; // Led vermelho
+
 int main () {
 	
 	
-printf("Hello, World!\n");
+while (true) {
+    
+    gpio_init(gpio_pin);
+    gpio_set_dir(gpio_pin, GPIO_OUT);
+    gpio_put(gpio_pin, 1);
+    sleep_ms(500);
+    gpio_put(gpio_pin, 0);
+    sleep_ms(500);
+    
+}
 
 
 return 0;
-
-	
-	
-	
-	
-}
